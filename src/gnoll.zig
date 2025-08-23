82bb5d8e3ea23fa2803529778821c72d82275f44
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2025-08-23 10:50:04 -0500

    make a bs doc change

--- src/gnoll.zig
+++ src/gnoll.zig
@@ -1,9 +1,7 @@
-//! By convention, root.zig is the root source file when making a library. If
-//! you are making an executable, the convention is to delete this file and
-//! start with main.zig instead.
 const std = @import("std");
 const testing = std.testing;
 
+/// This is a bullshit zig doc thing
 pub export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
