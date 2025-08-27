a5d362046d0bfd378675e836c3596cc2c0d9238f
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2025-08-26 23:10:57 -0500

    unmanaged arraylist and stringhashmap

--- src/gnoll.zig
+++ src/gnoll.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const testing = std.testing;
 const assert = std.debug.assert;
 const posix = std.poxix;
+const log = std.log.scoped(.Gnoll);
 
 pub const ConfigValue = union(enum) {
     bool: bool,
@@ -11,7 +12,7 @@ pub const ConfigValue = union(enum) {
     i16: i16,
     i32: i32,
     i64: i64,
-    str: []const u8,
+    bytes: []const u8,
     u128: u128,
     u16: u16,
     u32: u32,
@@ -24,44 +25,44 @@ pub const Gnoll = struct {
 
     allocator: std.mem.Allocator,
     options: GnollOptions,
-    config_options: std.array_list.Managed(ConfigOption),
-    config: std.StringHashMap(ConfigValue),
-    defaults: std.StringHashMap(ConfigValue),
+    config_options: std.ArrayList(ConfigOption),
+    config: std.StringHashMapUnmanaged(ConfigValue),
+    defaults: std.StringHashMapUnmanaged(ConfigValue),
 
     pub fn init(allocator: std.mem.Allocator, options: GnollOptions) Self {
         return Self{
             .allocator = allocator,
             .options = options,
-            .config_options = std.array_list.Managed(ConfigOption).init(allocator),
-            .config = std.StringHashMap(ConfigValue).init(allocator),
-            .defaults = std.StringHashMap(ConfigValue).init(allocator),
+            .config_options = .empty,
+            .config = .empty,
+            .defaults = .empty,
         };
     }
 
     pub fn deinit(self: *Self) void {
-        self.config_options.deinit();
+        self.config_options.deinit(self.allocator);
 
         var config_iter = self.config.valueIterator();
         while (config_iter.next()) |entry| {
             const value = entry.*;
 
             switch (value) {
-                .str => |s| self.allocator.free(s),
+                .bytes => |s| self.allocator.free(s),
                 else => {},
             }
         }
-        self.config.deinit();
+        self.config.deinit(self.allocator);
 
         var defaults_iter = self.defaults.valueIterator();
         while (defaults_iter.next()) |entry| {
             const value = entry.*;
 
             switch (value) {
-                .str => |s| self.allocator.free(s),
+                .bytes => |s| self.allocator.free(s),
                 else => {},
             }
         }
-        self.defaults.deinit();
+        self.defaults.deinit(self.allocator);
     }
 
     pub fn addConfigOption(self: *Self, filepath: []const u8, config_type: ConfigType) !void {
@@ -76,7 +77,7 @@ pub const Gnoll = struct {
             if (existing_option.eql(option)) return error.DuplicateConfigOption;
         }
 
-        try self.config_options.append(option);
+        try self.config_options.append(self.allocator, option);
     }
 
     pub fn set(self: *Self, comptime T: type, key: []const u8, value: T) !void {
@@ -84,7 +85,7 @@ pub const Gnoll = struct {
             bool => .{ .bool = value },
             []const u8 => blk: {
                 const duped = try self.allocator.dupe(u8, value);
-                break :blk .{ .str = duped };
+                break :blk .{ .bytes = duped };
             },
             f32 => .{ .f32 = value },
             f64 => .{ .f64 = value },
@@ -100,11 +101,11 @@ pub const Gnoll = struct {
             else => @compileError("Unsupported type in set()"),
         };
 
-        std.debug.print("current config count {}, capacity: {}\n", .{
+        std.debug.print("current config count {d}, capacity: {d}\n", .{
             self.config.count(),
             self.config.capacity(),
         });
-        try self.config.put(key, val_to_store);
+        try self.config.put(self.allocator, key, val_to_store);
     }
 
     pub fn setDefault(self: *Self, comptime T: type, key: []const u8, value: T) !void {
@@ -123,12 +124,12 @@ pub const Gnoll = struct {
             u8 => .{ .u8 = value },
             []const u8 => blk: {
                 const duped = try self.allocator.dupe(u8, value);
-                break :blk .{ .str = duped };
+                break :blk .{ .bytes = duped };
             },
             else => @compileError("Unsupported type in set()"),
         };
 
-        try self.defaults.put(key, val_to_store);
+        try self.defaults.put(self.allocator, key, val_to_store);
     }
 
     pub fn getAs(self: *Self, comptime T: type, key: []const u8) ?T {
@@ -141,7 +142,7 @@ pub const Gnoll = struct {
                 .i16 => |v| if (T == i16) v else null,
                 .i32 => |v| if (T == i32) v else null,
                 .i64 => |v| if (T == i64) v else null,
-                .str => |v| if (T == []const u8) v else null,
+                .bytes => |v| if (T == []const u8) v else null,
                 .u128 => |v| if (T == u128) v else null,
                 .u16 => |v| if (T == u16) v else null,
                 .u32 => |v| if (T == u32) v else null,
@@ -162,7 +163,7 @@ pub const Gnoll = struct {
                 .i16 => |v| if (T == i16) v else null,
                 .i32 => |v| if (T == i32) v else null,
                 .i64 => |v| if (T == i64) v else null,
-                .str => |v| if (T == []const u8) v else null,
+                .bytes => |v| if (T == []const u8) v else null,
                 .u128 => |v| if (T == u128) v else null,
                 .u16 => |v| if (T == u16) v else null,
                 .u32 => |v| if (T == u32) v else null,
@@ -291,9 +292,9 @@ test "read a config file" {
 
     try testing.expectEqual(123, u8_val);
 
-    try gnoll.set([]const u8, "str", "hello there");
-    const str_val = gnoll.getAs([]const u8, "str").?;
-    try testing.expect(std.mem.eql(u8, "hello there", str_val));
+    try gnoll.set([]const u8, "bytes", "hello there");
+    const bytes_val = gnoll.getAs([]const u8, "bytes").?;
+    try testing.expect(std.mem.eql(u8, "hello there", bytes_val));
 
     try gnoll.setDefault(u32, "default", 54321);
     const default_val = gnoll.getAs(u32, "default").?;
@@ -303,68 +304,4 @@ test "read a config file" {
     try gnoll.set(bool, "default_overridden", true);
     const default_val_overridden_val = gnoll.getAs(bool, "default_overridden").?;
     try testing.expectEqual(true, default_val_overridden_val);
-}
-
-test "casting" {
-    const allocator = testing.allocator;
-
-    const types = [_]type{
-        bool,
-        []const u8,
-        f32,
-        f64,
-        i128,
-        i16,
-        i32,
-        i64,
-        u128,
-        u16,
-        u32,
-        u64,
-        u8,
-    };
-
-    var gnoll = Gnoll.init(allocator, .{});
-    defer gnoll.deinit();
-
-    var tmp_key_buffer: [32]u8 = undefined;
-
-    inline for (0..types.len) |i| {
-        const T = types[i];
-        const key = try std.fmt.bufPrint(&tmp_key_buffer, "key_{}", .{i});
-
-        switch (T) {
-            u8, i16, u16, i32, u32, i64, u64, i128, u128 => {
-                std.debug.print("key {s} T: {any}\n", .{ key, T });
-                try gnoll.set(T, key, 1);
-
-                const v = gnoll.getAs(T, key).?;
-                std.debug.print("value {} T: {any}\n", .{ v, T });
-
-                try testing.expectEqual(1, v);
-            },
-            f32, f64 => {
-                std.debug.print("key {s} T: {any}\n", .{ key, T });
-                try gnoll.set(T, key, 1.0);
-                const v = gnoll.getAs(T, key).?;
-
-                try testing.expectEqual(1.0, v);
-            },
-            bool => {
-                std.debug.print("key {s} T: {any}\n", .{ key, T });
-                try gnoll.set(T, key, true);
-                const v = gnoll.getAs(T, key).?;
-
-                try testing.expectEqual(true, v);
-            },
-            []const u8 => {
-                std.debug.print("key {s} T: {any}\n", .{ key, T });
-                try gnoll.set(T, key, "hello there");
-                const v = gnoll.getAs(T, key).?;
-
-                try testing.expect(std.mem.eql(u8, "hello there", v));
-            },
-            else => unreachable,
-        }
-    }
 }
