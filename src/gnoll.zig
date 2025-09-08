eac2fcd259fab682939ee1e1ef2e8d4bb6a3ef38
Ben Hodge <ben.hodge@plusonerobotics.com>
2025-09-08 17:06:23 -0500

    better init

--- src/gnoll.zig
+++ src/gnoll.zig
@@ -24,75 +24,26 @@ const ConfigInfo = struct {
 
 pub const GnollOptions = struct {
     config_infos: []const ConfigInfo = &.{},
-};
 
-pub const Untyped = struct {};
-
-pub const Gnoll = struct {
-    const Self = @This();
-
-    const Config = struct {
-        config_info: ConfigInfo,
-        parsed: *anyopaque,
-        parsed_type: type,
-
-        pub fn init(comptime parsed_type: type, parsed: *anyopaque, config_info: ConfigInfo) Config {
-            return Config{
-                .parsed_type = parsed_type,
-                .parsed = parsed,
-                .config_info = config_info,
-            };
-        }
-    };
-
-    // config: Config,
-
-    pub fn init(comptime T: type, allocator: std.mem.Allocator, options: GnollOptions) !Self {
-        _ = T;
-        _ = allocator;
-        _ = options;
-        // try Gnoll.validateOptions(options);
-        // const config_info = try Gnoll.getConfigInfo(options);
-
-        // const config = try Gnoll.readConfig(T, allocator, config_info);
-        // errdefer config.deinit();
-
-        // const config = try options.parse(allocator, config_info);
-        // errdefer allocator.destroy(config);
-
-        return Self{
-            // .allocator = allocator,
-            // .gnoll_config = options,
-            // .config_info = config_info,
-            // .config = Config,
-        };
-    }
-
-    pub fn deinit(self: *Self, allocator: std.mem.Allocator) void {
-        _ = self;
-        _ = allocator;
-        // self.config_candidates.deinit(self.allocator);
-    }
-
-    fn validateOptions(options: GnollOptions) !void {
-        if (options.config_infos.len == 0) return error.MissingConfigInfo;
+    pub fn validate(self: GnollOptions) !void {
+        if (self.config_infos.len == 0) return error.MissingConfigInfo;
 
         var i: usize = 0;
-        while (i < options.config_infos.len) : (i += 1) {
-            if (i + 1 > options.config_infos.len) break; // we have exhausted the list, we are good
-            const current = options.config_infos[i];
+        while (i < self.config_infos.len) : (i += 1) {
+            if (i + 1 > self.config_infos.len) break; // we have exhausted the list, we are good
+            const current = self.config_infos[i];
 
-            for (i + 1..options.config_infos.len) |next_idx| {
-                if (current.eql(options.config_infos[next_idx])) return error.DuplicateConfigInfo;
+            for (i + 1..self.config_infos.len) |next_idx| {
+                if (current.eql(self.config_infos[next_idx])) return error.DuplicateConfigInfo;
             }
         }
     }
 
-    fn getConfigInfo(options: GnollOptions) !ConfigInfo {
-        if (options.config_infos.len == 0) return error.MissingConfigCandidate;
+    pub fn getConfigInfo(self: GnollOptions) !ConfigInfo {
+        if (self.config_infos.len == 0) return error.MissingConfigCandidate;
 
         // for each candidate
-        for (options.config_infos) |candidate| {
+        for (self.config_infos) |candidate| {
             // Check if the file exists
             log.debug("checking path {s}\n", .{candidate.filepath});
             _ = std.fs.cwd().statFile(candidate.filepath) catch |err| switch (err) {
@@ -114,45 +65,100 @@ pub const Gnoll = struct {
 
         return error.NoEligibleConfigInfoFound;
     }
+};
 
-    fn readConfig(comptime T: type, allocator: std.mem.Allocator, config_info: ConfigInfo) !Config {
-        // figure out if the file exists or return an error
-        const file = std.fs.cwd().openFile(config_info.filepath, .{}) catch |err| {
-            log.err("File '{s}' does not exist\n", .{config_info.filepath});
-            return err;
+pub const Untyped = struct {};
+
+pub fn Gnoll(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        const Config = struct {
+            config_info: ConfigInfo,
+            parsed: *anyopaque,
+            parsed_type: type,
+
+            pub fn init(comptime parsed_type: type, parsed: *anyopaque, config_info: ConfigInfo) Config {
+                return Config{
+                    .parsed_type = parsed_type,
+                    .parsed = parsed,
+                    .config_info = config_info,
+                };
+            }
         };
-        defer file.close();
 
-        const stat = try file.stat();
-        const file_size = stat.size;
+        // config: Config,
 
-        const buf = try allocator.alloc(u8, file_size);
-        defer allocator.free(buf);
+        pub fn init(allocator: std.mem.Allocator, options: GnollOptions) !Self {
+            try options.validate();
+            const config_info = try options.getConfigInfo();
 
-        const n = try file.readAll(buf);
+            log.err("config_info {any}", .{config_info});
 
-        if (n != file_size) return error.UnexepectedFileReadError;
+            _ = allocator;
+            // try Gnoll.validateOptions(options);
+            // const config_info = try Gnoll.getConfigInfo(options);
 
-        // figure out how to parse this config
-        // if this is a json config
-        switch (config_info.format) {
-            .json => {
-                const parsed: json.Parsed(T) = try json.parseFromSlice(T, allocator, buf, .{});
-                errdefer parsed.deinit();
+            // const config = try Gnoll.readConfig(T, allocator, config_info);
+            // errdefer config.deinit();
 
-                return Config.init(json.Parsed(T), parsed, parsed.value);
+            // const config = try options.parse(allocator, config_info);
+            // errdefer allocator.destroy(config);
 
-                // return Config{
-                //     .parsed = parsed,
-                //     .value = parsed.value,
-                // };
-            },
-            else => unreachable,
+            return Self{
+                // .allocator = allocator,
+                // .gnoll_config = options,
+                // .config_info = config_info,
+                // .config = Config,
+            };
         }
 
-        // return Config{};
-    }
-};
+        pub fn deinit(self: *Self, allocator: std.mem.Allocator) void {
+            _ = self;
+            _ = allocator;
+            // self.config_candidates.deinit(self.allocator);
+        }
+
+        fn readConfig(allocator: std.mem.Allocator, config_info: ConfigInfo) !Config {
+            // figure out if the file exists or return an error
+            const file = std.fs.cwd().openFile(config_info.filepath, .{}) catch |err| {
+                log.err("File '{s}' does not exist\n", .{config_info.filepath});
+                return err;
+            };
+            defer file.close();
+
+            const stat = try file.stat();
+            const file_size = stat.size;
+
+            const buf = try allocator.alloc(u8, file_size);
+            defer allocator.free(buf);
+
+            const n = try file.readAll(buf);
+
+            if (n != file_size) return error.UnexepectedFileReadError;
+
+            // figure out how to parse this config
+            // if this is a json config
+            switch (config_info.format) {
+                .json => {
+                    const parsed: json.Parsed(T) = try json.parseFromSlice(T, allocator, buf, .{});
+                    errdefer parsed.deinit();
+
+                    return Config.init(json.Parsed(T), parsed, parsed.value);
+
+                    // return Config{
+                    //     .parsed = parsed,
+                    //     .value = parsed.value,
+                    // };
+                },
+                else => unreachable,
+            }
+
+            // return Config{};
+        }
+    };
+}
+
 test "basic workflow" {
     const allocator = testing.allocator;
 
@@ -177,7 +183,7 @@ test "basic workflow" {
         },
     };
 
-    var gnoll = try Gnoll.init(MyConfigFileType, allocator, gnoll_options);
+    var gnoll = try Gnoll(MyConfigFileType).init(allocator, gnoll_options);
     defer gnoll.deinit(allocator);
 
     // try testing.expectEqual(0, gnoll.config_candidates.items.len);
